5a38107d5ee292db0f6ebdf571a9250fbe391549
holdera <holdera2@hotmail.com>
2024-05-07 07:02:56 -0400

    feat: adds animation in and fix for nav link click

--- src/app/components/Layout/Nav/Navigation.js
+++ src/app/components/Layout/Nav/Navigation.js
@@ -2,18 +2,39 @@ import { useState } from 'react';
 import { useLockScroll } from '../../../utils/LockScroll';
 import MenuButton from './MenuButton';
 import Link from 'next/link';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Navigation({ menuLinks }) {
 	const [open, setOpen] = useState(false);
 
 	const { lockScroll, unLockScroll } = useLockScroll();
 
-	const windowScroll = open ? lockScroll() : unLockScroll();
+	open ? lockScroll() : unLockScroll();
 
 	function handleMenuButtonClick() {
 		setOpen((prevState) => !prevState);
-		//windowScroll;
 	}
+
+	function handleLinkClick() {
+		setOpen(false);
+	}
+
+	const variants = {
+		open: {
+			left: '0vw',
+			transition: {
+				type: 'spring',
+				stiffness: 30,
+			},
+		},
+		close: {
+			left: '100vw',
+			transition: {
+				type: 'spring',
+				stiffness: 30,
+			},
+		},
+	};
 
 	return (
 		<>
@@ -21,26 +42,32 @@ export default function Navigation({ menuLinks }) {
 				onMenuButtonClick={handleMenuButtonClick}
 				isOpen={open}
 			/>
-			{open && (
-				<nav
-					id='site-navigation'
-					className='fixed top-0 left-0 w-full h-screen bg-white z-30 text-center pt-16'
-				>
-					{' '}
-					<ul>
-						{menuLinks.map((link, i) => (
-							<li key={`link-${i}`} className='mb-5'>
-								<Link
-									className='antialiased text-black font-bold text-2xl md:text-3xl'
-									href={link.href}
-								>
-									{link.text}
-								</Link>
-							</li>
-						))}
-					</ul>
-				</nav>
-			)}
+			<AnimatePresence>
+				{open && (
+					<motion.nav
+						animate='open'
+						className='fixed top-0 -left-[100vw] w-full h-screen bg-white z-30 text-center pt-16'
+						exit='close'
+						id='site-navigation'
+						variants={variants}
+					>
+						{' '}
+						<ul>
+							{menuLinks.map((link, i) => (
+								<li key={`link-${i}`} className='mb-5'>
+									<Link
+										className='antialiased text-black font-bold text-2xl md:text-3xl'
+										href={link.href}
+										onClick={handleLinkClick}
+									>
+										{link.text}
+									</Link>
+								</li>
+							))}
+						</ul>
+					</motion.nav>
+				)}
+			</AnimatePresence>
 		</>
 	);
 }
